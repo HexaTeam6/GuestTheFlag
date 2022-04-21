f39dae83cc82b84bb7e130bff5ca4104e3e19213
HexaTeam6 <abdurrachmanwahed@gmail.com>
2022-04-22 00:41:01 +0700

    Initial Commit

---
+++ GuestTheFlag/GuestTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuestTheFlagApp.swift
+//  GuestTheFlag
+//
+//  Created by Abdur Rachman Wahed on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct GuestTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
